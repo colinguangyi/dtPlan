bff224b6e5b99caf261d94e9d6288e5c18f330b0
colin0702 <colin0702@163.com>
2020-08-10 11:11:46 +0800

    去除测试内容

---
+++ server/src/test/java/SpringTest.java
@@ -0,0 +1,10 @@
+import com.colin.server.Server;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Server.class)// 指定启动类
+public class SpringTest {
+
+}
